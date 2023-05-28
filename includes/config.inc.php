fabc60bf7cd37a1b6cd08f31f3d69eb13f28989e
Klimán László <gamf.kolesz@gmail.com>
2023-05-28 09:51:19 +0200

    config.inc.php

---
+++ includes/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+$ablakcim = array(
+    'cim' => '',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.jpg',
+    'kepalt' => 'logo',
+	'cim' => 'Szakemberkereső',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Készitette: Szebelédi Mariann (C919WN & Klimán László (MS7G8P) '
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'koszonto', 'szoveg' => 'Köszöntő', 'menun' => array(1,1)),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'szakik' => array('fajl' => 'szakik', 'szoveg' => 'Szakik', 'menun' => array(1,1)),
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+    'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
